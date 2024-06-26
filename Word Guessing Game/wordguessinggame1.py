da948b113231809481a91f4172ec8bec112d4340
Andy <andyduong03@gmail.com>
2024-06-25 21:55:47 -0700

    Word Guessing Game 1

---
+++ Word Guessing Game/wordguessinggame1.py
@@ -0,0 +1,46 @@
+# Small word guessing game from Python. The user needs to guess the
+# word within the minimum number of guesses.
+# Source: https://www.geeksforgeeks.org/python-program-for-word-guessing-game/
+
+import random
+
+name = input("What is your name? ")
+print(f'Good Luck! {name}')
+print("Guess the characters")
+
+# List of words
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+
+# Get a random word from the list, number of turns, and a 'guesses' variable
+# to store the user guesses
+random_word = random.choice(words)
+turns = 12
+guesses = ''
+
+# While loop when the user has more than 0 turns
+while turns > 0:
+    guessed_correctly = 0   # Variable to check how many words are not guessed left
+    
+    for x in random_word:   # Loop through the random word
+        if x in guesses:    # Check if the user guessed the word correctly
+            print(x, end=" ")
+        else:               # The user guessed incorrectly
+            print("_", end=" ")
+            guessed_correctly += 1  # +1 to 'guessed_correctly' for each unguessed character
+        
+    if guessed_correctly == 0:    # The user guessed all the characters in the word
+        print(f'\nYou Won! The word is {random_word}')
+        break
+
+    user_guess = input("\nGuess a character: ")
+    guesses += user_guess
+
+    if user_guess not in random_word:   # Check if the character the user guessed is not in the word
+        turns -= 1
+        print(f'Wrong. You have {turns} turns left.')   # Reduce a turn
+
+# The user failed to guess the word
+if turns == 0:
+    print(f'You Lost! The word is {random_word}')
