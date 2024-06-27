b3c69db4e61f1a873d67f59d1652a7e01522d864
Andy <andyduong03@gmail.com>
2024-06-26 19:55:42 -0700

    Word Guessing Game

--- Word Guessing Game/wordguessinggame1.py
+++ Word Guessing Game/wordguessinggame1.py
@@ -1,5 +1,8 @@
-# Small word guessing game from Python. The user needs to guess the
-# word within the minimum number of guesses.
+# In this game, there is a list of words present, out of which our interpreter will choose 1 random word. 
+# The user first has to input their names and then, will be asked to guess any alphabet. 
+# If the random word contains that alphabet, it will be shown as the output(with correct placement) 
+# else the program will ask you to guess another alphabet. 
+# The user will be given 12 turns(which can be changed accordingly) to guess the complete word.
 # Source: https://www.geeksforgeeks.org/python-program-for-word-guessing-game/
 
 import random
